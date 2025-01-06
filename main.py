1c0cddb70b80c7f174372bf6a963a343d42e2a86
Eisman333 <28490588+Eisman333@users.noreply.github.com>
2025-01-06 21:59:05 +0100

    refactor book report

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    chars_list = get_chars_list(text)
-    chars_list.sort(reverse=True, key=sort_on)
-
-    print_report(book_path, num_words, chars_list)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 def get_book_text(path):
     with open(path) as f:
@@ -17,20 +16,27 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
-def get_chars_list(text):
-    chars = []
+def get_chars_dict(text):
+    chars = {}
     text = text.lower()
     for char in alc:
-        chars.append({"char": char, "num": text.count(char)})
+        chars[char] = text.count(char)
     return chars
 
 def sort_on(dict):
     return dict["num"]
 
+def chars_dict_to_sorted_list(chars_dict):
+    sorted_list = []
+    for char in chars_dict:
+        sorted_list.append({"char": char, "num": chars_dict[char]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
 def print_report(book, word_count, chars_list):
     print(f"--- Begin report of {book} ---")
     print(f"{word_count} words found in the book")
-    print("")
+    print()
     for char in chars_list:
         print(f"The '{char["char"]}' character was found {char["num"]} times")
     print("--- End report ---")

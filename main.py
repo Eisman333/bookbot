0a7985a23a67d5da67b9586c6de4d78217c737f3
Eisman333 <28490588+Eisman333@users.noreply.github.com>
2025-01-06 21:43:45 +0100

    character counting

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from string import ascii_lowercase as alc
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the book")
+    chars_dict = get_chars_dict(text)
+    print(chars_dict)
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,5 +14,12 @@ def get_book_text(path):
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_chars_dict(text):
+    chars = {}
+    text = text.lower()
+    for char in alc:
+        chars[char] = text.count(char)
+    return chars
 
 main()
